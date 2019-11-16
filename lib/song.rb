9995542117e6bc8fb490b2b8317bc0abce26d7b5
Chelsea Hansen <chelseahansen@gmail.com>
2019-11-16 02:25:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,4 +6,8 @@ class Song
     @genre = genre
     genre.add_song(self)
   end
+  madonna = Artist.new("Madonna")
+pop = Genre.new("pop")
+ 
+into_the_groove = Song.new("Into the Groove", madonna, pop)
 end
