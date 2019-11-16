2a45c587d10ed327c664b26c489f9ab3c7455b43
Chelsea Hansen <chelseahansen@gmail.com>
2019-11-16 02:25:58 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,5 +6,8 @@ class Appointment
     @doctor = doctor
     doctor.add_appointment(self)
   end
-
+ferris = Patient.new("Ferris Bueller")
+doogie = Doctor.new("Doogie Howser")
+ 
+new_appointment = Appointment.new('January 1st, 1989', ferris, doogie)
 end
